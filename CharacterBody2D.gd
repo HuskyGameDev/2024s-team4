e9b03610678987e1e2a4d4796b097e959c8a0d0d
Relic <rjburns@mtu.edu>
2024-03-17 14:33:13 -0400

    Squashed commit of the following:
    commit dabd075e287a39c3537842ead2d2abde29b9f99b
    Author: timeturtle21 <cartersoccer08@gmail.com>
    Date:   Thu Feb 22 23:06:40 2024 -0500
    
        Adding some projectile and basic player

---
+++ CharacterBody2D.gd
@@ -0,0 +1,40 @@
+extends CharacterBody2D
+
+const bulletPath = preload("res://bullet.tscn")
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		shoot()
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+		
+
+
+
+	move_and_slide()
+	
+	
+
+func shoot():
+	var bullet = bulletPath.instantiate()
+	
+	get_parent().add_child(bullet)
+	bullet.position = $Marker2D.global_position
+	
